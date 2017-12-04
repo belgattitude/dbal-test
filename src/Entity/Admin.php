77ba5e40f1ccdc83ccdf67a6cd5c7e8e7ecc020a
Denis Golubovskiy <bukashk0zzz@me.com>
2017-12-04 13:00:44 +0200

    init

---
+++ src/Entity/Admin.php
@@ -0,0 +1,126 @@
+<?php declare(strict_types = 1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Admin
+ *
+ * @ORM\Table(name="admins")
+ * @ORM\Entity()
+ */
+class Admin
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned": true})
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255, nullable=true)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=true)
+     */
+    private $password;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="status", type="boolean", nullable=false, options={"unsigned": true, "default" : "1"})
+     */
+    private $status = true;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     *
+     * @return Admin
+     */
+    public function setLogin(string $login): Admin
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Admin
+     */
+    public function setPassword(string $password): Admin
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set status
+     *
+     * @param bool $status
+     *
+     * @return Admin
+     */
+    public function setStatus(bool $status): Admin
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return bool
+     */
+    public function getStatus(): bool
+    {
+        return $this->status;
+    }
+}
---
+++ up.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker-compose up --remove-orphans --build -d
+
+docker-compose exec --user root php rm -rf /var/www/html/var/cache/dev /var/www/html/var/cache/test
+docker-compose exec --user www-data php chmod -R 777 /var/www/html/var
+
+docker-compose exec --user www-data php bin/console doctrine:database:drop -n --force
+docker-compose exec --user www-data php bin/console doctrine:database:create -n
+docker-compose exec --user www-data php bin/console doctrine:migrations:migrate -n
